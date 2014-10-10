e89bdd9304b21df86e5c044ce43268f7bfd22c85
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2014-10-10 23:14:22 +0200

    Updated various dependencies

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
 (defproject clj-west "0.0.1-SNAPSHOT"
   :description "A barebones WS wrapper for Datomic"
   :url "http://github.com/vrivellino/clojure-west-demo"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.3"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.apache.httpcomponents/httpclient "4.3.1"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.apache.httpcomponents/httpclient "4.3.5"
                   :exclusions [commons-logging]]
-                 [ring/ring-core "1.2.0"]
-                 [compojure "1.1.5"]
-                 [com.datomic/datomic-pro "0.9.4609"]]
-  :plugins [[lein-ring "0.8.0"]]
+                 [ring/ring-core "1.3.1"]
+                 [compojure "1.2.0"]
+                 [com.datomic/datomic-pro "0.9.4956"
+                  :exclusions [joda-time]]]
+  :plugins [[lein-ring "0.8.12"]]
   ;; To access Datomic's maven repo, set the following environment variables:
   ;; export MY_DATOMIC_USERNAME=<USERNAME>
   ;; export MY_DATOMIC_PASSWORD=XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
